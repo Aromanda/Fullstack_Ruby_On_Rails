53d07bbfea1752e1a1ba83e672d92c8a0e051bc8
Aromanda <aromandanyfitiaratoejanahary@yahoo.fr>
2023-07-29 00:17:07 -0400

    edit employee fonctionne

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,52 +1,55 @@
 # app/controllers/employees_controller.rb
 class EmployeesController < ApplicationController
-    before_action :set_employee, only: [:show, :edit, :update, :destroy]
-  
-    def index
-      @employees = Employee.all
-    end
-  
-    def show
-    end
-  
-    def new
-      @employee = Employee.new
-    end
-  
-    def create
-      @employee = Employee.new(employee_params)
-  
-      if @employee.save
-        redirect_to @employee, notice: 'Employee was successfully created.'
-      else
-        render :new
-      end
-    end
-  
-    def edit
-    end
-  
-    def update
-      if @employee.update(employee_params)
-        redirect_to @employee, notice: 'Employee was successfully updated.'
-      else
-        render :edit
-      end
-    end
-  
-    def destroy
-      @employee.destroy
-      redirect_to employees_url, notice: 'Employee was successfully deleted.'
-    end
-  
-    private
-  
-    def set_employee
-      @employee = Employee.find(params[:id])
-    end
-  
-    def employee_params
-      params.require(:employee).permit(:user.name, :email, :phone)
+  before_action :authenticate_user! # Assuming you are using Devise for authentication
+  before_action :set_employee, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+  end
+
+  def new
+    @employee = Employee.new
+    @employee.build_user # Create an associated user object for the new employee
+  end
+
+  def create
+    @employee = Employee.new(employee_params)
+    if @employee.save
+      redirect_to @employee, notice: 'Employee was successfully created.'
+    else
+      render :new
     end
   end
+
+  def edit
+  end
+
+  def update
+    if @employee.update(employee_params)
+      redirect_to @employee, notice: 'Employee was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @employee.destroy
+    redirect_to employees_url, notice: 'Employee was successfully deleted.'
+  end
+
+  private
+
+  def set_employee
+    if params[:id]
+      @employee = Employee.find(params[:id])
+    end
+  end
+
+  def employee_params
+    params.require(:employee).permit(:phone, :email, user_attributes: [:id, :name])
+  end
+end
   
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,10 @@
+# app/models/employee.rb
 class Employee < ApplicationRecord
-  belongs_to :address
   belongs_to :user
-  validates :user_id, presence: true, uniqueness: true
-  validates :phone, presence: true
+  belongs_to :address
+
+  accepts_nested_attributes_for :user
+
+  validates :user_id, :address_id, presence: true
 end
 
